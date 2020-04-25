6807522792ed3b9daa5e643a037de53b96f0bb88
cortesana <a.chdez2020@gmail.com>
2020-04-25 16:20:29 +0200

    Revision commit

--- src/components/PokeList.js
+++ src/components/PokeList.js
@@ -5,7 +5,7 @@ function PokeList(props) {
     return(
         <ul className="pokeList">
             {props.pokeListData.map(pokeListData => {
-                return <li className="Pokemon">
+                return <li className="Pokemon" key={pokeListData.id}>
                     <Pokemon
                         pokeImg = {pokeListData.url}
                         pokeName = {pokeListData.name}
@@ -18,5 +18,11 @@ function PokeList(props) {
     );
 }
 
+Pokemon.propTypes = {
+    id: PropTypes.string,
+    url: PropTypes.string,
+    name: PropTypes.string,
+    types: PropTypes.string
+}
 
 export default PokeList;
--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -3,13 +3,19 @@ import React from 'react';
 function Pokemon(props) {
     return(
         <div className="pokeContainer">
-            <img className="pokeImg" alt="Pokemon" src={props.pokeImg}/>
+            <img className="pokeImg" alt={props.pokeName} src={props.pokeImg}/>
             <h2 className="pokeName">{props.pokeName}</h2>
             <ul className="pokeTypes">
                 {props.pokeTypesArray.map((pokeType, index) => <li className="pokeType" key={index}>{pokeType}</li>)}
             </ul>
         </div>
     );
+}
+
+Pokemon.propTypes = {
+    pokeImg: PropTypes.string,
+    pokeName: PropTypes.string,
+    pokeType: PropTypes.string
 }
 
 export default Pokemon;
